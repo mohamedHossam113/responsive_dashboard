16d510f407a55264daf5109796d1e20668000122
mohamedHossam113 <rues185@gmail.com>
2025-07-23 12:06:08 +0300

    icons,images and fonts uploaded

---
+++ responsive_dashboard/lib/views/dashoard_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/widgets/dashboar_desktop_layout.dart';
+
+class DashoardView extends StatelessWidget {
+  const DashoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+          mobileLayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DashboarDesktopLayout()),
+    );
+  }
+}
---
+++ responsive_dashboard/lib/widgets/dashboar_desktop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboarDesktopLayout extends StatelessWidget {
+  const DashboarDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
