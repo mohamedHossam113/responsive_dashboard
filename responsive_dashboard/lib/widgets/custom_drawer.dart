af29bf4f65a762c9308e48674213e430f4382819
mohamedHossam113 <rues185@gmail.com>
2025-07-24 16:19:24 +0300

    updates

--- responsive_dashboard/lib/widgets/custom_drawer.dart
+++ responsive_dashboard/lib/widgets/custom_drawer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/utils/constants.dart';
 import 'package:responsive_dashboard/utils/drawer_item_list.dart';
 import 'package:responsive_dashboard/widgets/active_and_inactive_items.dart';
-import 'package:responsive_dashboard/widgets/drawer_items_list.dart';
+import 'package:responsive_dashboard/widgets/drawer_items_listview.dart';
 import 'package:responsive_dashboard/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
